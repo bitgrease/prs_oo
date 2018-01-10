ecf055f63396f2799f4de7c3eef92de0274b58ae
Kelly Netterville <bitgrease@gmail.com>
2018-01-10 11:51:56 -0500

    Initial class skeleton

---
+++ prs_oo.rb
@@ -0,0 +1,66 @@
+# Paper Rock Scissors
+# User makes choice
+# Computer makes choice
+# Winner is displayed
+
+=begin
+  LS Description:
+  Rock, Paper, Scissors is a two-player game where each player chooses
+one of three possible moves: rock, paper, or scissors. The chosen moves
+will then be compared to see who wins, according to the following rules:
+
+- rock beats scissors
+- scissors beats paper
+- paper beats rock
+
+If the players chose the same move, then it's a tie.
+
+Nouns: player, move, rule
+Verbs: choose, compare
+
+Player
+- choose
+Move
+Rule
+
+-compare
+=end
+
+
+class Player
+  def initialize
+
+  end
+
+  def choose
+
+  end
+end
+
+
+class Move
+  def initialize
+
+  end
+end
+
+class Rule
+  def initialize
+
+  end
+end
+
+def compare(move1, move2)
+
+end
+
+
+class RPSGame
+  def initialize
+
+  end
+
+  def play
+
+  end
+end
