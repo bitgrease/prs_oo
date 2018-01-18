4aa595bfa740e7a0ac83e3df6a04b22a540fa305
Kelly Netterville <bitgrease@gmail.com>
2018-01-17 21:22:06 -0500

    Added ScoreBoard class and implemented match play to first 10 wins.

--- prs_oo.rb
+++ prs_oo.rb
@@ -3,14 +3,43 @@
  Maybe make a ScoreBoard class that takes the player scores. Scoreboard can output and format the score. Scoreboard can
  show the winner too if it's told the winner's name.
 
- RPSGame class still determines winner. Sends info to ScorBoard instance for formatting and output.
+ RPSGame class still determines winner. Sends info to ScoreBoard instance for formatting and output.
 
 =end
-  
-end
-
 
 require 'pry'
+
+class ScoreBoard
+
+  def initialize(name_one, name_two)
+    @name_one = name_one
+    @name_two = name_two
+
+    @scores = {
+      name_one => 0,
+      name_two => 0
+    }
+  end
+
+  def increase_score(player_name)
+    @scores[player_name] += 1
+  end
+
+  def display_score
+    @scores.each do |name, score|
+      puts "#{name} has #{score} points"
+    end
+  end
+
+  def winner?
+    @scores.values.any? { |score| score.eql?(10) }
+  end
+
+  def get_winner
+    @scores.key(10)
+  end
+end
+
 class Move
   VALUES = %w[rock paper scissors]
 
@@ -48,16 +77,11 @@ class Move
 end
 
 class Player
-  attr_accessor :move, :name, :score
-
-  def initialize
-    self.score = 0
-  end
+  attr_accessor :move, :name
 end
 
 class Human < Player
   def initialize
-    super
     set_name
   end
 
@@ -100,28 +124,16 @@ class Computer < Player
 end
 
 class RPSGame
-  attr_accessor :human, :computer
+  attr_accessor :human, :computer, :score_board
 
   def initialize
     @human = Human.new
     @computer = Computer.new
+    @score_board = ScoreBoard.new(human.name, computer.name)
   end
 
   def display_welcome_message
     puts "Hi #{human.name}! Welcome to Rock, Paper, Scissors!"
-  end
-
-  def display_score
-    if (human.score == computer.score)
-      puts "Sorry, no winner. It was a tie."
-      return
-    end
-
-    winner = human.score > computer.score ? human : computer
-
-    p "Score was #{human.name} : #{human.score}"
-    puts " and #{computer.name} : #{computer.score}"
-    puts "#{winner.name} wins!"
   end
 
   def display_player_choices
@@ -130,19 +142,21 @@ class RPSGame
   end
 
   def display_winner
+    human_name = human.name
+    computer_name = computer.name
     if human.move > computer.move
-      puts "#{human.name} won!"
-      human.score += 1
+      puts "#{human_name} won!"
+      score_board.increase_score(human_name)
     elsif human.move < computer.move
-      puts "#{computer.name} won."
-      computer.score += 1
+      puts "#{computer_name} won!"
+      score_board.increase_score(computer_name)
     else
       puts "It's a tie."
     end
   end
 
   def display_overall_winner
-    puts "#{[human, computer].find {|p| p.score == 10}.name} is the overall winner!"
+    puts "#{score_board.get_winner} is the overall winner!"
   end
 
   def play_again?
@@ -162,20 +176,24 @@ class RPSGame
 
   def play
     display_welcome_message
+    score_board.display_score
 
     loop do
       human.choose
       computer.choose
       display_player_choices
       display_winner
-      binding.pry
-      break if overall_winner?
-      break unless play_again? 
-      display_score
+      sleep 2
+
+      system('cls') || system('clear')
+      score_board.display_score
+      break if score_board.winner?      
+      break unless play_again?
     end
-    display_score
+    system('cls') || system('clear')     
     display_overall_winner
   end
 end
 
+system('cls') || system('clear') 
 RPSGame.new.play
